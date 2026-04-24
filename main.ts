d9b27af86aee8e272cfa731253a88f05f22ba108
DangHoangLong <long.711.sg@gmail.com>
2026-04-24 22:21:41 +0700

    use Text - joint text

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 basic.showIcon(IconNames.Fabulous)
 basic.pause(200)
 basic.forever(function () {
-    lcd.displayText("MKE-S01", 1, 1)
-    lcd.displayText("Distance:" + ultraSonic.readDistance(ultraSonic.PingUnit.Centimeters, ultraSonic.PinKit.Port1) + "cm", 1, 2)
+    basic.showIcon(IconNames.Diamond)
+    lcd.displayText("MKE-S01", 5, 1)
+    lcd.displayText("Distance: " + ultraSonic.readDistance(ultraSonic.PingUnit.Centimeters, ultraSonic.PinKit.Port1) + "cm", 1, 2)
     basic.pause(500)
+    basic.showIcon(IconNames.SmallDiamond)
 })
