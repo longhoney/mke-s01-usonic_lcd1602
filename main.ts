aa6acec7b15a7b52e36c02f023ac28e704e43a15
DangHoangLong <long.711.sg@gmail.com>
2026-05-03 08:33:13 +0700

    delay 1s

--- main.ts
+++ main.ts
@@ -2,5 +2,5 @@ basic.forever(function () {
     lcd.clearScreen()
     lcd.displayText("MKE-S01 (cm):", 1, 1)
     lcd.displayText(convertToText(ultraSonic.readDistance(ultraSonic.PingUnit.Centimeters, ultraSonic.PinKit.Port1)), 1, 2)
-    basic.pause(500)
+    basic.pause(1000)
 })
